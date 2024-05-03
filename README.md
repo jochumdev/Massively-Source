5e9881ca7d4ee0ff5882dc488fa8a22abec04995
René Jochum <rene@jochum.dev>
2024-05-03 07:55:45 +0200

    feat(all): Update to Massively-Source

--- README.md
+++ README.md
@@ -1,64 +1,63 @@
-# Massively
+# Massively-source
 
-This is Massively, a text-heavy, article-oriented design built around a huge background
-image and scroll effects powered by Scrollex. Originally created by [@ajlkn](https://twitter.com/ajlkn) for [HTML5 UP](https://html5up.net) and later ported to [Ghost](https://ghost.org)
+This is Massively-source, a text-heavy, article-oriented design built around a huge background
+image and scroll effects powered by Scrollex. Originally created by [@ajlkn](https://twitter.com/ajlkn) for [HTML5 UP](https://html5up.net) and later ported to [Ghost](https://ghost.org), then merged with the [Source](https://github.com/TryGhost/Source) theme by [jochumdev](https://jochum.dev).
 
-**Demo: https://massively.ghost.io**
+**Demo: https://rene.jochum.dev**
 
-&nbsp;
+## Features 
 
-![image](https://user-images.githubusercontent.com/120485/49293031-7276b000-f4e1-11e8-8b71-43dc53c67f00.png)
+### for users
 
+- Fast
+- Works well without JavaScript
+- No CDN
+- Parallax effect background image
+- Automatic higlighting of code in Markdown
 
-&nbsp;
+### for creators / developers
 
-# First time using a Ghost theme?
+- No external CDN dependencies, host all on your own:
+    - @tryghost/sodo-Search, @tryghost/portal GPR compatible
+    - Source Sans Pro, Merriweather fonts GPR compatible (no cdn)
+- No jQuery
+- scss using dart-sass
+- Parallax effect background using jarallax
+- Customizeable over theme custom parameters
+- Highlight.js
+- Automaticaly updates and copies all code using "npm" and Gulp
+- single .js for all source with the help of Gulp
 
-Ghost uses a simple templating language called [Handlebars](http://handlebarsjs.com/) for its themes.
+## Known Bugs/Todos
 
-We've documented our default theme pretty heavily so that it should be fairly easy to work out what's going on just by reading the code and the comments. Once you feel comfortable with how everything works, we also have full [theme API documentation](https://themes.ghost.org) which explains every possible Handlebars helper and template.
+- Implement Dark/Light color switcher with a `config.custom` for the default
+- Test everything with javascript disabled
+- Remove all css which isn't needed
+- Fix feature_image sizes
+- Make Font switching work
+- Use the asset helper in `default.hbs`
+- Fix errors/warnings found by gscan.
+- i18n using [polyglot.js](https://github.com/airbnb/polyglot.js)
+- Benchmark with regulary with [webpagetest.org](https://www.webpagetest.org/)
+- Integrate and remove [ghost.scss](assets/scss/layout/_ghost.scss)
+- Upgrade all deps in package.json using `ncu -a` and fix gulpfile.js
+- Maybe use rollup instead/together with Gulp.
+- Implement https://ghost.org/docs/themes/helpers/asset/ - hasMinFile
 
-**The main files are:**
+## Development
 
-- `default.hbs` - The main template file
-- `index.hbs` - Used for the home page
-- `post.hbs` - Used for individual posts
-- `page.hbs` - Used for individual pages
-- `tag.hbs` - Used for tag archives
-- `author.hbs` - Used for author archives
+1. Install and run a ghost site localy
+2. Install this theme over air
+3. Remove the theme on disc inside site123/content/themes/massively-source
+4. Symlink it to the deleted folder
+5. run `npm run dev`
 
-One neat trick is that you can also create custom one-off templates just by adding the slug of a page to a template file. For example:
+## Authors
 
-- `page-about.hbs` - Custom template for the `/about/` page
-- `tag-news.hbs` - Custom template for `/tag/news/` archive
-- `author-ali.hbs` - Custom template for `/author/ali/` archive
+- [@ajlkn](https://twitter.com/ajlkn)
+- Ghost Foundation
+- [@jochumdev](https://jochum.dev)
 
+## Copyright & License
 
-# Development
-
-This implementation tries to stay as true as possible to the original template without making too many modifications. The original code is unmodified, preserving the ability to update it later.
-
-There are two main changes compared to the original template files:
-
-- The original template contained separate `/assets` and `/images` directories. Ghost Themes require that all assets be nested under a top-level `/assets` directory, so these are moved to `/assets/main` and `/assets/images`, respectively.
-- In order to make minor modifications and add some new custom styles, one additional SaSS file is added under `/assets/main/sass/layout/ghost.sass` and included at the bottom of the `main.sass` file.
-
-To work on styles in this theme, you'll need to run a local development environment to build/watch for changes. Once cloned and installed with npm/yarn, the following `gulp` build tasks are available:
-
-```bash
-# Build files locally and watch for changes
-gulp
-
-# Build production zip locally and save to /dist
-gulp zip
-
-# Run compatibility test against latest version of Ghost
-yarn test
-```
-
-Original template files and design by [@ajlkn](https://twitter.com/ajlkn)
-
-
-# Copyright & License
-
-Copyright (c) 2013-2022 [HTML5 UP](https://htmlup.net) & [Ghost Foundation](https://ghost.org) - This theme is licensed under both the [MIT and Creative Commons Attribution 3.0](LICENSE). Please note that the terms of the Creative Commons license require that you maintain the footer attribution to freely use this theme.
+Copyright (c) 2013-2024 Ghost Foundation - Released under the [MIT license](LICENSE).
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,26 @@
-const {series, watch, src, dest, parallel} = require('gulp');
+const { series, watch, src, dest, parallel, task } = require('gulp');
 const pump = require('pump');
+const path = require('path');
+const releaseUtils = require('@tryghost/release-utils');
+const inquirer = require('inquirer');
 
 // gulp plugins and utils
-var livereload = require('gulp-livereload');
-var sass = require('gulp-sass')(require('node-sass'));
-var zip = require('gulp-zip');
-var beeper = require('beeper');
+const livereload = require('gulp-livereload');
+const zip = require('gulp-zip');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const gcopy = require('gulp-copy');
+const beeper = require('beeper');
+const fs = require('fs');
+var merge = require('merge-stream');
+
+// sass
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-dart-sass');
+
+const REPO = 'TryGhost/Source';
+const REPO_READONLY = 'TryGhost/Source';
+const CHANGELOG_PATH = path.join(process.cwd(), '.', 'changelog.md');
 
 function serve(done) {
     livereload.listen();
@@ -23,44 +38,155 @@ const handleError = (done) => {
 
 function hbs(done) {
     pump([
-        src(['*.hbs', 'partials/**/*.hbs', '!node_modules/**/*.hbs']),
+        src(['*.hbs', 'partials/**/*.hbs']),
         livereload()
     ], handleError(done));
 }
 
-sass.compiler = require('node-sass');
+function copy(done) {
+    [
+        src('node_modules/@tryghost/sodo-search/umd/main.css').pipe(dest('assets/jsdelivr/sodo-search')),
+        src('node_modules/@tryghost/sodo-search/umd/sodo-search.min.js').pipe(dest('assets/jsdelivr/sodo-search')),
+        src('node_modules/@tryghost/portal/umd/portal.min.js').pipe(dest('assets/jsdelivr/')),
+        src('node_modules/@fontsource/source-sans-pro/**/*').pipe(dest('assets/fonts/source-sans-pro')),
+        src('node_modules/@fontsource/merriweather/**/*').pipe(dest('assets/fonts/merriweather')),
+        done(),
+    ];
+}
 
 function css(done) {
     pump([
-        src('./assets/main/sass/*.scss', {sourcemaps: true}),
-        sass({outputStyle: 'compressed'}).on('error', sass.logError),
-        dest('assets/main/css', {sourcemaps: './'}),
+        src('assets/scss/*.scss'),
+        // sass({outputStyle: 'compressed', includePaths: ['node_modules']}).on('error', sass.logError),
+        sass({ includePaths: ['node_modules'] }).on('error', sass.logError),
+        dest('assets/built', { sourcemaps: './' }),
+        livereload()
+    ], handleError(done));
+}
+
+function js(done) {
+    pump([
+        src([
+            // node modules
+            'node_modules/jarallax/dist/jarallax.js',
+            'node_modules/imagesloaded/imagesloaded.pkgd.js',
+            'node_modules/photoswipe/dist/umd/photoswipe.umd.min.js',
+            'node_modules/photoswipe/dist/umd/photoswipe-lightbox.umd.min.js',
+
+            'assets/highlightjs/highlight.min.js',
+            'assets/highlightjs/styles/*.min.js',
+
+            // pull in lib files first so our own code can depend on it
+            'assets/js/lib/*.js',
+            'assets/js/*.js'
+        ], { sourcemaps: true }),
+        concat('source.js'),
+        // uglify(),
+        dest('assets/built/', { sourcemaps: '.' }),
         livereload()
     ], handleError(done));
 }
 
 function zipper(done) {
-    var targetDir = 'dist/';
-    var themeName = require('./package.json').name;
-    var filename = themeName + '.zip';
+    const filename = require('./package.json').name + '.zip';
 
     pump([
         src([
             '**',
+            '!.git', '!.git/**',
             '!node_modules', '!node_modules/**',
-            '!dist', '!dist/**'
+            '!dist', '!dist/**',
+            '!yarn-error.log',
+            '!yarn.lock',
+            '!gulpfile.js'
         ]),
         zip(filename),
-        dest(targetDir)
+        dest('dist/')
     ], handleError(done));
 }
 
-const cssWatcher = () => watch('./assets/main/sass/**/**', css);
-const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs', '!node_modules/**/*.hbs'], hbs);
-const watcher = parallel(cssWatcher, hbsWatcher);
-const build = series(css);
-const dev = series(build, serve, watcher);
+const cssWatcher = () => watch('assets/scss/**/**', css);
+const jsWatcher = () => watch('assets/js/**', js);
+const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs'], hbs);
+const watcher = parallel(cssWatcher, jsWatcher, hbsWatcher);
+const build = series(copy, css, js);
 
 exports.build = build;
 exports.zip = series(build, zipper);
-exports.default = dev;
+exports.default = series(build, serve, watcher);
+
+exports.release = async () => {
+    // @NOTE: https://yarnpkg.com/lang/en/docs/cli/version/
+    // require(./package.json) can run into caching issues, this re-reads from file everytime on release
+    let packageJSON = JSON.parse(fs.readFileSync('./package.json'));
+    const newVersion = packageJSON.version;
+
+    if (!newVersion || newVersion === '') {
+        console.log(`Invalid version: ${newVersion}`);
+        return;
+    }
+
+    console.log(`\nCreating release for ${newVersion}...`);
+
+    const githubToken = process.env.GST_TOKEN;
+
+    if (!githubToken) {
+        console.log('Please configure your environment with a GitHub token located in GST_TOKEN');
+        return;
+    }
+
+    try {
+        const result = await inquirer.prompt([{
+            type: 'input',
+            name: 'compatibleWithGhost',
+            message: 'Which version of Ghost is it compatible with?',
+            default: '5.0.0'
+        }]);
+
+        const compatibleWithGhost = result.compatibleWithGhost;
+
+        const releasesResponse = await releaseUtils.releases.get({
+            userAgent: 'Source',
+            uri: `https://api.github.com/repos/${REPO_READONLY}/releases`
+        });
+
+        if (!releasesResponse || !releasesResponse) {
+            console.log('No releases found. Skipping...');
+            return;
+        }
+
+        let previousVersion = releasesResponse[0].tag_name || releasesResponse[0].name;
+        console.log(`Previous version: ${previousVersion}`);
+
+        const changelog = new releaseUtils.Changelog({
+            changelogPath: CHANGELOG_PATH,
+            folder: path.join(process.cwd(), '.')
+        });
+
+        changelog
+            .write({
+                githubRepoPath: `https://github.com/${REPO}`,
+                lastVersion: previousVersion
+            })
+            .sort()
+            .clean();
+
+        const newReleaseResponse = await releaseUtils.releases.create({
+            draft: true,
+            preRelease: false,
+            tagName: 'v' + newVersion,
+            releaseName: newVersion,
+            userAgent: 'Source',
+            uri: `https://api.github.com/repos/${REPO}/releases`,
+            github: {
+                token: githubToken
+            },
+            content: [`**Compatible with Ghost ≥ ${compatibleWithGhost}**\n\n`],
+            changelogPath: CHANGELOG_PATH
+        });
+        console.log(`\nRelease draft generated: ${newReleaseResponse.releaseUrl}\n`);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+};
