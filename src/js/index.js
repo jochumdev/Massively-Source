9597f202d571c0bae743491e94f982a68b8f2520
René Jochum <rene@jochum.dev>
2024-05-05 19:24:17 +0200

    fix(javascript): Rework init.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,16 @@
 import functional from "./src/functional";
-import { i18n, localize } from "./src/i18n";
+import i18n from "./src/i18n";
 import settings from "./src/settings";
 import util from "./src/util";
 
-export default { functional, i18n, localize, settings, util };
+// This intializes the theme.
+// It has to be called AFTER settings has been populated.
+async function init() {
+    // Await i18n;
+    await i18n.init();
+
+    // None i18n stuff.
+    await functional.init();
+};
+ar
+export default { init, settings, util };
--- src/js/src/i18n.js
+++ src/js/src/i18n.js
@@ -18,57 +18,33 @@ import settings from "./settings";
 //     }
 // })
 i18n.use(HttpBackend)
-    .use(Cache)
-    .use(LanguageDetector);
+  .use(Cache)
+  .use(LanguageDetector);
+
+// create a loc-i18next localizesator.
+var localize = locI18next.init(i18n, {
+  selectorAttr: 'data-i18n', // selector for translating elements
+  targetAttr: 'i18n-target',
+  optionsAttr: 'i18n-options',
+  useOptionsAttr: true,
+  parseDefaultValueFromContent: true,
+  document: window.document,
+});
 
 
-i18n.myInit = function() {
-    if (i18n.isInitialized || i18n.isInitializing) {
-        return i18n.promise;
-    }
-
-    i18n.promise = theme.i18n.init({
-        ns: settings.Get(settings.KEYS.I18N_NAMESPACES),
-        backend: {
-            loadPath: '/assets/i18n/\{{lng}}/\{{ns}}.json',
-        },
-        caches: ['localStorage'],
-        detection: {
-            order: ['querystring', 'path', 'localStorage', 'sessionStorage'],
-            lookupQueryString: 'lang',
-            lookupFromPathIndex: 0,
-
-            // optional conversion function to use to modify the detected language code
-            convertDetectedLanguage: 'Iso15897',
-            convertDetectedLanguage: (lng) => {
-                const l = lng.replace('-', '_');
-                if (settings.Get(settings.KEYS.I18N_LANGUAGES).includes(l)) {
-                    return l;
-                }
-
-                return settings.Get(settings.KEYS.I18N_DEFAULT_LANGUAGE);
-            }
-        },
-        useDataAttrOptions: true,
-        interpolation: {
-            escapeValue: false // to allow interpolation in HTML
-        },
-    });
-
-    return i18n.promise;
-}
-
-i18n.myInit = function () {
-  if (i18n.isInitialized || i18n.isInitializing) {
-    // Update <html lang="??">
-    i18n.promise.then(function() {
-        document.documentElement.setAttribute('lang', i18n.language);
-    });
-
-    return i18n.promise;
+let promise;
+async function init() {
+  if (promise) {
+    return promise;
   }
 
-  i18n.promise = theme.i18n.init({
+  await settings.promise;
+
+  if (!settings.Get(settings.KEYS.I18N)) {
+    return new Promise.resolve();
+  }
+
+  promise = i18n.init({
     ns: settings.Get(settings.KEYS.I18N_NAMESPACES),
     backend: {
       loadPath: "/assets/i18n/{{lng}}/{{ns}}.json",
@@ -90,18 +66,65 @@ i18n.myInit = function () {
         return settings.Get(settings.KEYS.I18N_DEFAULT_LANGUAGE);
       },
     },
+    useDataAttrOptions: true,
+    interpolation: {
+      escapeValue: false // to allow interpolation in HTML
+    },
   });
 
-  return i18n.promise;
+  // Wait for i18next to be ready.
+  await promise;
+
+  // Update <html lang="??">
+  document.documentElement.setAttribute('lang', i18n.language);
+
+  //
+  // Hack to implement language switching.
+  // TODO(jochumdev): find a better way!
+  //
+  // Update nav menus.
+  const language = i18n.language;
+  const navs = [document.getElementById('nav'), document.getElementById('panel-nav')];
+
+  for (let n = 0; n < navs.length; n++) {
+    var navLinks = navs[n].querySelectorAll('.links>li');
+
+    var activeEL = null;
+    for (let i = 0; i < navLinks.length; i++) {
+      var el = navLinks[i];
+      var link = new URL(el.firstChild.getAttribute("href"), window.location.origin);
+      var langLink = "/" + language + "/" + link.pathname.substring(4)
+      if (theme.util.pathIsCurrent(langLink)) {
+        activeEL = el;
+      }
+      el.firstChild.setAttribute("href", langLink);
+    };
+
+    if (activeEL != null) {
+      activeEL.classList.add("active");
+    }
+  };
+
+  // Localize the theme.
+  localize('.i18n-target');
 };
 
-var localize = locI18next.init(i18n, {
-    selectorAttr: 'data-i18n', // selector for translating elements
-    targetAttr: 'i18n-target',
-    optionsAttr: 'i18n-options',
-    useOptionsAttr: true,
-    parseDefaultValueFromContent: true,
-    document: window.document,
-});
+async function updateNav() {
+  // Wait for settings to be ready.
+  await settings.promise;
 
-export { i18n, localize };
+  const settings = theme.settings;
+  const SKEYS = settings.KEYS;
+
+  // Create language switchers.
+  const langs = settings.Get(SKEYS.I18N_LANGUAGES);
+  var switchers = '';
+  langs.forEach(function (lang) {
+    switchers += '<li class="gh-langselect ' + lang + '"><a href="/' + lang + '/">' + '<img src="/assets/images/i18n/' + lang + '.png" alt="' + lang + '" /></a></li>';
+  });
+
+  var switcher = document.querySelector('#gh-language-switcher ul');
+  switcher.innerHTML = switchers;
+}
+
+export default { localize, init, updateNav };
